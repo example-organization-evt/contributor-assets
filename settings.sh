7c47373e00a4abc6ae30dda094492405b5e44756
Nathan Ladd <nathanladd@gmail.com>
2024-07-15 20:43:35 -0500

    settings.sh includes Rubygems authority data

--- settings.sh
+++ settings.sh
@@ -1,19 +1,13 @@
 home_env_var="EXAMPLE_ORGANIZATION_HOME"
-default_git_authority_path="git@github.com:example-organization-evt"
-default_git_branch="master"
-default_git_remote_name="origin"
+git_authority_path="git@github.com:example-organization-evt"
+git_default_branch="master"
+git_remote_name="origin"
+rubygems_authority_path="http://localhost:8080"
+rubygems_authority_access_key="example-organization"
 
 if [ -z "${!home_env_var:-}" ]; then
   printf "\e[31mError: $home_env_var is not set\e[39m\n"
   return 1
-fi
-
-PROJECTS_HOME=${PROJECTS_HOME:-${!home_env_var}}
-if [ "$PROJECTS_HOME" != "${!home_env_var}" ]; then
-  printf "\e[31mError: $PROJECTS_HOME is not set to ${!home_env_var}; exiting unsuccessfully\e[39m"
-  return 1
-else
-  export PROJECTS_HOME
 fi
 
 if [ -z "${LIBRARIES_HOME:-}" ]; then
@@ -21,20 +15,33 @@ if [ -z "${LIBRARIES_HOME:-}" ]; then
   return 1
 fi
 
-if [ "${GIT_AUTHORITY_PATH:-}" != "$default_git_authority_path" ]; then
+if [ "${PROJECTS_HOME:=${!home_env_var}}" != "${!home_env_var}" ]; then
+  printf "\e[31mError: $PROJECTS_HOME is not set to ${!home_env_var}; exiting unsuccessfully\e[39m"
+  return 1
+fi
+export PROJECTS_HOME
+
+if [ "${GIT_AUTHORITY_PATH:=$git_authority_path}" != "$git_authority_path" ]; then
   printf "\e[33mWarning: GIT_AUTHORITY_PATH is set: $GIT_AUTHORITY_PATH\e[39m\n"
-else
-  export GIT_AUTHORITY_PATH="$default_git_authority_path"
 fi
+export GIT_AUTHORITY_PATH
 
-if [ "${GIT_REMOTE_NAME:-}" != "$default_git_remote_name" ]; then
+if [ "${GIT_REMOTE_NAME:=$git_remote_name}" != "$git_remote_name" ]; then
   printf "\e[33mWarning: GIT_REMOTE_NAME is set: $GIT_REMOTE_NAME\e[39m\n"
-else
-  export GIT_REMOTE_NAME="$default_git_remote_name"
 fi
+export GIT_REMOTE_NAME
 
-if [ "${GIT_DEFAULT_BRANCH:-}" != "$default_git_branch" ]; then
+if [ "${GIT_DEFAULT_BRANCH:=$git_default_branch}" != "$git_default_branch" ]; then
   printf "\e[33mWarning: GIT_DEFAULT_BRANCH is set: $GIT_DEFAULT_BRANCH\e[39m\n"
-else
-  export GIT_DEFAULT_BRANCH="$default_git_branch"
 fi
+export GIT_DEFAULT_BRANCH
+
+if [ "${RUBYGEMS_AUTHORITY_PATH:=$rubygems_authority_path}" != "$rubygems_authority_path" ]; then
+  printf "\e[33mWarning: RUBYGEMS_AUTHORITY_PATH is set: $RUBYGEMS_AUTHORITY_PATH\e[39m\n"
+fi
+export RUBYGEMS_AUTHORITY_PATH
+
+if [ "${RUBYGEMS_AUTHORITY_ACCESS_KEY:=$rubygems_authority_access_key}" != "$rubygems_authority_access_key" ]; then
+  printf "\e[33mWarning: RUBYGEMS_AUTHORITY_ACCESS_KEY is set: $RUBYGEMS_AUTHORITY_ACCESS_KEY\e[39m\n"
+fi
+export RUBYGEMS_AUTHORITY_ACCESS_KEY
